b6b16b8f368d91a7eda2de057c639496984f3af5
Maksym Denysov <93475429+Javac-g@users.noreply.github.com>
2026-03-09 23:34:35 +0200

    Update Vertex.java

--- Vertex.java
+++ Vertex.java
@@ -1,5 +1,11 @@
-class Vertex {
-    double x;
-    double y;
-    int id;
+public final class Vertex {
+    public final int id;
+    public final double x;
+    public final double y;
+
+    public Vertex(int id, double x, double y) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+    }
 }
