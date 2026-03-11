a581fac488ec3d5bb98636ea2af6725e320b94ff
Maksym Denysov <93475429+Javac-g@users.noreply.github.com>
2026-03-11 22:14:29 +0200

    Encapsulate Vertex properties with getter methods

--- Vertex.java
+++ Vertex.java
@@ -1,11 +1,18 @@
+package com.max.hexopt.core.topology;
+
 public final class Vertex {
-    public final int id;
-    public final double x;
-    public final double y;
+
+    private final int id;
+    private final double x;
+    private final double y;
 
     public Vertex(int id, double x, double y) {
         this.id = id;
         this.x = x;
         this.y = y;
     }
+
+    public int id() { return id; }
+    public double x() { return x; }
+    public double y() { return y; }
 }
